2f6f221603a45d5b5c74d95ec9c9dbaf58bb479b
NotAlex575 <dapontealessandro89gmail.com>
2025-07-08 20:10:30 +0200

    4) Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+console.log(function_tot_caratteri(word))
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+function function_tot_caratteri(word){
+    let vocali = [];
+    let contatore = 0; 
+    for(let i = 0; i < word.length; i++){
+        if(word[i] == "a" || word[i] == "e" || word[i] == "i" || word[i] == "o" || word[i] == "u"){
+            contatore++;
+            vocali.push(word[i])
+        }
+    }
+    return contatore + " " + vocali;
+}
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
