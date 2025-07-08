99eb7e7a194a268d455546c9826b3da11b5d6bb8
NotAlex575 <dapontealessandro89gmail.com>
2025-07-08 20:20:40 +0200

    EXTRA: Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,22 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+const tempo = prompt("che ore sono?")
+console.log(function_saluto(name, tempo));
 
 // Invoca la funzione qui e stampa il risultato in console
 
+function function_saluto(name, tempo){
+    if(tempo < 13){
+        return "buongiorno "+name;
+    }
+    else if (tempo > 13 && tempo < 17){
+        return "buonpomeriggio "+name
+    }
+    else if(tempo > 17 && tempo < 24){
+        return "buonasera "+name
+    }
+}
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
