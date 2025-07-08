2e5cee8af51a4a40bec60e0624285f6c5824cf35
NotAlex575 <dapontealessandro89gmail.com>
2025-07-08 20:00:38 +0200

    3) Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,13 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const lettera = prompt("quale lettera vogliamo ritornare? ")
+console.log(function_lettera(names, lettera))
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+function function_lettera(names, lettera){
+    return names.filter(name => name.charAt(0) == lettera);
+}
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
