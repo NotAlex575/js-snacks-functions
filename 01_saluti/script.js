eeec0e5c0ffb83c6676fbb4ebf28c6ece8747d29
NotAlex575 <dapontealessandro89gmail.com>
2025-07-08 19:40:14 +0200

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+
+function function_username(userName){
+    return "ciao " + userName;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(function_username(userName));
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
